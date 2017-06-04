a7918cb889ba7d1c893279a5676ce1b085e9d059
Steven Ireland <scientastersteve@gmail.com>
2017-06-04 18:09:29 -0400

    make whispersocket serve built client for real

--- index.js
+++ index.js
@@ -42,9 +42,7 @@ io.on('connection', function(socket){
 
 });
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/', 'index.html'));
-});
+app.use(express.static('public'));
 
 http.listen(port, function(){
   console.log('listening on localhost:' + port);
